3c2782cc5a97579414664a3dd03d914dafeb0c1b
DannyBoyNYC <daniel.deverell@gmail.com>
2019-07-30 12:28:31 -0400

    corrected config

--- src/config/config.js
+++ src/config/config.js
@@ -10,14 +10,4 @@ export const endpoint = `https://api.github.com/graphql`;
 
 export const readmeEndpoint = `http://api.github.com/repos/front-end-summer19/ExpressJS/readme`;
 
-// .then(res => console.log(res.data.user.organizations.edges[0]));
-
-// .then(res => console.log(res.data.organization.repositories.edges));
-
-{
-  /* <pre>
-          <code>{JSON.stringify(props, null, 2)}</code>
-        </pre> */
-}
-
 export default options;
